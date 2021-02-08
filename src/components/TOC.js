49f904199d2ee5f81ebac61e0709c60db9ac7667
YusunPark <solarpy@hanyang.ac.kr>
2021-02-08 18:13:31 +0900

    Add event

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,22 +2,30 @@ import React, { Component } from "react";
 
 class TOC extends Component {
   render() {
+    console.log("TOC render");
     var lists = [];
-    var data = this.props.data
+    var data = this.props.data;
     var i = 0;
-    while(i < data.length) {
-      lists.push(<li key={data[i].id}><a href={"/content/" + data[i].id}>{data[i].title}</a></li>)
-      i = i+1;
+    while (i < data.length) {
+      lists.push(
+        <li key={data[i].id}>
+          <a href={"/content/" + data[i].id}
+          onClick={function(e) {
+            e.preventDefault();
+            this.props.onChangePage(e.target.dataset.id);
+          }.bind(this)}
+          >{data[i].title}</a>
+        </li>
+      );
+      i = i + 1;
     }
 
     return (
       <nav>
-        <ul>
-          {lists}
-        </ul>
+        <ul>{lists}</ul>
       </nav>
     );
   }
 }
 
-export default TOC; 
+export default TOC;
