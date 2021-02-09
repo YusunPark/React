7450b451911d1a2ee34feba870121b25ae621074
YusunPark <solarpy@hanyang.ac.kr>
2021-02-09 18:20:43 +0900

    Add create function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import TOC from "./components/TOC";
-import Content from "./components/Content";
+// import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
 import Subject from "./components/Subject";
+import Control from "./components/Control";
 import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.max_content_id = 3;
     this.state = {
       mode: "read",
       selected_content_id : 2,
@@ -21,24 +25,37 @@ class App extends Component {
   }
 
   render() {
-    var _title,
-      _desc = null;
+    var _title, _desc, _article = null;
+
     if (this.state.mode === "welcome") {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
     } else if (this.state.mode === "read") {
       var i = 0;
-      while(i < this.state.contents.length){
+      while (i < this.state.contents.length) {
         var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id){
+        if (data.id === this.state.selected_content_id) {
           _title = data.title;
           _desc = data.desc;
+          _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+
           break;
         }
         i = i + 1;
       }
-      
-    }
+    } else if (this.state.mode === "create") {
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        this.max_content_id = this.max_content_id + 1;
+        var _contents = this.state.contents.concat(
+          {id:this.max_content_id, title:_title, desc:_desc}
+        );
+        this.setState({
+          contents:_contents
+        });
+        console.log(_title, _desc);
+      }.bind(this)}></CreateContent>;
+    } 
     return (
       <div className="App">
         <Subject
@@ -57,7 +74,14 @@ class App extends Component {
             selected_content_id : Number(id)
           });
         }.bind(this)} data={this.state.contents}></TOC>
-        <Content title={_title} desc={_desc}></Content>
+        {/* <Content title={_title} desc={_desc}></Content> */}
+        <Control onChangeMode={function(_mode){
+          this.setState({
+            mode:_mode
+          })
+        }.bind(this)}>
+        </Control>
+        {_article}
       </div>
     );
   }
---
+++ src/components/Control.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class Control extends Component {
+  render() {
+    return(
+      <ul>
+        <li><a href="/create" onClick={function(e){
+          console.log('create successfully')
+          e.preventDefault();
+          this.props.onChangeMode('create');
+        }.bind(this)}>create</a></li>
+
+        <li><a href="/update" onClick={function(e){
+          console.log("update successfully");
+
+          e.preventDefault();
+          this.props.onChangeMode('update');
+        }.bind(this)}>update</a></li>
+
+        <li><input onClick={function(e){
+          console.log("delete successfully");
+
+          e.preventDefault();
+          this.props.onChangeMode('delete');
+        }.bind(this)} type='button' value="delete"></input></li>
+      </ul>
+    )
+  }
+}
+
+export default Control;
